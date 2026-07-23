8b507a8dcbf7b139d682476249f88ea4f15c38a8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-23 23:08:18 +0800

    use the new json extract method

--- src/test/Program.vb
+++ src/test/Program.vb
@@ -1,11 +1,14 @@
 Imports System
+Imports Microsoft.VisualBasic.MIME.application.json
+Imports Microsoft.VisualBasic.MIME.application.json.LenientJson
 Imports Ollama
+Imports OmicsAgent
 
 Module Program
     Sub Main(args As String())
         Dim respo As New LLMsResponse With {.output = "G:\OmicsWorks\test\plan_response.txt".ReadAllText}
         Dim json As String = respo.ExtractJsonFromResponse
-
+        Dim plan As ModulePlan = LenientJsonParser.ParseJSON(json).CreateObject(Of ModulePlan)
         Pause()
     End Sub
 End Module
