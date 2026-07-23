1f9ec77ae67193c5646988ed16ca2acb179a65bc
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-24 03:17:22 +0800

    Update run_agent.cmd

--- test/metabolism/run_agent.cmd
+++ test/metabolism/run_agent.cmd
@@ -2,4 +2,4 @@
 
 set agent="G:\OmicsWorks\agent\bin\research.exe"
 
-CALL %agent% -r=research.txt -e=expression.csv -a=metabolites.csv -s=sampleinfo.csv -k=./pubmed/ -w=./demo/ -c=config.ini --skip-literature
+CALL %agent% -r=research.txt -e=expression.csv -a=metabolites.csv -s=sampleinfo.csv -k=./pubmed/ -w=./demo/ -c=config.ini --skip-literature --skip-kb --module 1,2,3,4,6,7,8,9
