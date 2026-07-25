b2e4f9b9db80d973b604ddf631ca78884d728e0f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-25 09:29:50 +0800

    make improvements of the json parssing from llm response

--- test/metabolism/run_agent.cmd
+++ test/metabolism/run_agent.cmd
@@ -2,4 +2,5 @@
 
 set agent="G:\OmicsWorks\agent\bin\research.exe"
 
-CALL %agent% -r=research.txt -e=expression.csv -a=metabolites.csv -s=sampleinfo.csv -k=./pubmed/ -w=./demo/ -c=config.ini --skip-literature --skip-kb 
+CALL %agent% -r=research.txt -e=expression.csv -a=metabolites.csv -s=sampleinfo.csv -k=./pubmed/ -w=./demo/ -c=config.ini 
+REM --skip-literature --skip-kb 
