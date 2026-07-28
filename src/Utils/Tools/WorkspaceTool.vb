5f5a73c9cf0c6345dd0f6a937dee29c266bf8bba
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-28 16:53:18 +0800

    make improvements of the aganet workspace tool

---
+++ src/Utils/Tools/WorkspaceTool.vb
@@ -0,0 +1,21 @@
+﻿Public MustInherit Class WorkspaceTool
+
+    Protected ReadOnly _workspaceRoot As String
+    Protected ReadOnly _logger As Action(Of String)
+
+    Public Sub New(workspaceRoot As String, Optional logger As Action(Of String) = Nothing)
+        _workspaceRoot = workspaceRoot
+        _logger = If(logger, AddressOf Console.WriteLine)
+    End Sub
+
+    Protected Overridable Function ResolvePath(relativePath As String) As String
+        If String.IsNullOrWhiteSpace(relativePath) Then Return ""
+        If Path.IsPathRooted(relativePath) Then Return relativePath
+        Return Path.GetFullPath(Path.Combine(_workspaceRoot, relativePath))
+    End Function
+
+    Protected Shared Function EscapeJson(input As String) As String
+        If String.IsNullOrEmpty(input) Then Return ""
+        Return input.Replace("\", "\\").Replace("""", "\""").Replace(vbCr, "\r").Replace(vbLf, "\n").Replace(vbTab, "\t")
+    End Function
+End Class
