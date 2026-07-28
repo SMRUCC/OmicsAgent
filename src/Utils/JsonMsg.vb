44004f8a88e86134ba7370c9c5cdff82b85d00c8
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-28 16:38:07 +0800

    fix of the json error message

---
+++ src/Utils/JsonMsg.vb
@@ -0,0 +1,8 @@
+﻿Imports Microsoft.VisualBasic.Serialization.JSON
+
+Module JsonMsg
+
+    Public Function [error](msg As String) As String
+        Return New Dictionary(Of String, String) From {{"error", msg}}.GetJson
+    End Function
+End Module
