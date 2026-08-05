2e671961f52d55908f81d29b440bd63ca3a0c627
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-08-05 10:29:47 +0800

    analysais data sets

--- src/Models/Dataset/SampleAligner.vb
+++ src/Models/Dataset/SampleAligner.vb
@@ -1,6 +1,6 @@
 Imports Microsoft.VisualBasic.Data.Framework.StorageProvider
 
-Namespace AppRuntime
+Namespace Dataset
 
     ''' <summary>
     ''' 跨组学样本对齐执行器。
@@ -407,27 +407,6 @@ Namespace AppRuntime
             End If
 
             Return ""
-        End Function
-
-    End Class
-
-    ''' <summary>
-    ''' 单个生物学个体在各组学中的样本 ID 对应关系。
-    ''' </summary>
-    Public Class SubjectMapping
-
-        ''' <summary>统一的生物学个体标识</summary>
-        Public ReadOnly Property SubjectId As String
-
-        ''' <summary>组学 Id -> 该组学中对应的原始样本 ID</summary>
-        Public ReadOnly Property OmicsSample As New Dictionary(Of String, String)
-
-        Public Sub New(subjectId As String)
-            _SubjectId = subjectId
-        End Sub
-
-        Public Overrides Function ToString() As String
-            Return $"{SubjectId}: {String.Join(", ", OmicsSample.Select(Function(kv) $"{kv.Key}={kv.Value}"))}"
         End Function
 
     End Class
---
+++ src/Models/Dataset/SubjectMapping.vb
@@ -0,0 +1,23 @@
+﻿Namespace Dataset
+
+    ''' <summary>
+    ''' 单个生物学个体在各组学中的样本 ID 对应关系。
+    ''' </summary>
+    Public Class SubjectMapping
+
+        ''' <summary>统一的生物学个体标识</summary>
+        Public ReadOnly Property SubjectId As String
+
+        ''' <summary>组学 Id -> 该组学中对应的原始样本 ID</summary>
+        Public ReadOnly Property OmicsSample As New Dictionary(Of String, String)
+
+        Public Sub New(subjectId As String)
+            _SubjectId = subjectId
+        End Sub
+
+        Public Overrides Function ToString() As String
+            Return $"{SubjectId}: {String.Join(", ", OmicsSample.Select(Function(kv) $"{kv.Key}={kv.Value}"))}"
+        End Function
+
+    End Class
+End Namespace
